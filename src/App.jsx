0bb22334c945c746f10a2efd69afb5dea3bd4e85
Felipe Silva <felipe.silva@spsp.com.br>
2023-08-14 17:54:51 -0300

    Change colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import {Sidebar, SidebarItem} from './components/Sidebar';
-import { LifeBuoy, Receipt, Boxes, Package, UserCircle, BarChart3, LayoutDashboard, Settings, Box} from 'lucide-react';
+import Sidebar from './components/Sidebar';
 
 export default function App() {
-
   return (
     <main className='App'>
-      <Sidebar>
-          <SidebarItem icon={<LayoutDashboard  size={20}/>} text='Dashboard' alert />
-          <SidebarItem icon={<BarChart3 size={20} />} text='Statistics' active />
-          <SidebarItem icon={<UserCircle size={20} />} text='Users' />
-          <SidebarItem icon={<Boxes size={20} />} text='Inventory' />
-          <SidebarItem icon={<Package size={20} />} text='Orders' />
-          <SidebarItem icon={<Receipt size={20} />} text='Bilings' />
-          <hr className="my-3" />
-          <SidebarItem icon={<Settings size={20} />} text='Settings' />
-          <SidebarItem icon={<LifeBuoy size={20} />} text='Help' />
-      </Sidebar>
+      <Sidebar />
     </main>
   )
 }
